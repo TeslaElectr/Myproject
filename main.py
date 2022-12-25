94f7f5c9c4527544da112a7f1cb39c629326aae9
Ivan Lebedev <ivanleb171@gmail.com>
2022-12-25 20:33:10 +0300

    Add class Product def produce def move

---
+++ main.py
@@ -0,0 +1,23 @@
+class Product:
+    # # коммерческие и логистические переменные:
+    # cost = 340000
+    # storage = 'Saint-Petersburg'
+    #
+    # # Физико-химические переменные
+    # amine_number = 1300  # мгKOH на ммл
+    # color = 'brown'
+    # density = 1000  # кг на метр кубический
+
+    #  def produce(self, amount) amount - колличество проивзодимого продукта
+    #  def
+
+
+
+
+    def __init__(self):
+        pass
+
+    def produce(self, amount):
+        pass
+
+    def move(sef,):
