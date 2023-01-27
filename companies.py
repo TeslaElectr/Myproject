4bca618c2e7549da5691d2ae9b09d132f4790a21
Ivan Lebedev <ivanleb171@gmail.com>
2023-01-27 15:57:07 +0300

    Add @property in class companies.py

--- companies.py
+++ companies.py
@@ -10,17 +10,49 @@ class Company:
 
     #  Здесь вводятся реквизиты компании из карточки партнёра
     #  От этого класса, по замыслу, будет наследоваться класс Horda (название компании)
-    #
-    #
 
-    def __int__(self):
-        pass
+    def __int__(
+            self,
+            legal_address: str = None,
+            mailing_address: str = None,
+            contact_numbers: str = None,
+            INN: str = None,
+            KPP: str = None,
+            bank: str = None,
+            checking_account: str = None,
+            corr_account: str = None,
+            bik: str = None,
+            post_code: str = None
+    ):
+        self.legal_address = legal_address
+        self.mailing_address = mailing_address
+        self.contact_numbers = contact_numbers
+        self.INN = INN
+        self.KPP = KPP
+        self.bank = bank
+        self.checking_account = checking_account
+        self.corr_account = corr_account
+        self.bik = bik
+        self.post_code = post_code
+
+    @property
+    def get_legal_address(self):
+        return self.legal_address
+
+    @property
+    def get_mailing_address(self):
+        return self.mailing_address
+
+    @property
+    def get_contact_numbers(self):
+        return self.get_contact_numbers
+
+    @property
+    def get_contact_numbers(self):
+        return self.contact_numbers
 
 
 class Horda(Company):
-
-    def __int__(self):
-        pass
 
     def buy_product(self, name: str, value: float = 10.1, price: float = 100000, unit: str = 'rub'):
         # в этом методе я покупаю продукт. Припокупки создаётся экземпляр класса и
@@ -28,7 +60,7 @@ class Horda(Company):
         #
         # Сейчас тут представлен принцип работы этого замысла.
 
-        # Также нужно продукмать как сделать так что бы при вызове функции, он уже знал какие обекты уже сидят в учёте (учёт может
+        # Также нужно продумать как сделать так что бы при вызове функции, он уже знал какие обекты уже сидят в учёте (учёт может
         # быть любой: списки, словари, бд (БД я еще не умею))
         product_object = dict_of_products[name]
         exit = product_object.produce(product_object)
